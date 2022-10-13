14276612ea0fa3b4b489e928e9294afc0a39b662
jihun90 <jihun90.kim@icloud.com>
2022-10-13 22:41:21 +0900

    deltaTime 추가

--- Assets/Driver.cs
+++ Assets/Driver.cs
@@ -4,16 +4,16 @@ using UnityEngine;
 
 public class Driver : MonoBehaviour
 {
-    [SerializeField] float steerSpeed = 1;
-    [SerializeField] float moveSpeed = 0.01f;
+    [SerializeField] float steerSpeed = 100;
+    [SerializeField] float moveSpeed = 10f;
     void Start()
     {
     }
 
     void Update()
     {
-        float steerAmount = Input.GetAxis("Horizontal") * steerSpeed;
-        float moveAmount = Input.GetAxis("Vertical") * moveSpeed;
+        float steerAmount = Input.GetAxis("Horizontal") * steerSpeed * Time.deltaTime;
+        float moveAmount = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
         transform.Rotate(0, 0, -steerAmount);
         transform.Translate(0, moveAmount, 0);
     }
