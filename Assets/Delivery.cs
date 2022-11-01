554b0ef0a9f63ce0528cf7d141a9de45c093c458
jihun90 <jihun90.kim@icloud.com>
2022-11-01 22:37:37 +0900

    Package를 가지고 있을때는 새로운 Package를 가지지 못하도록 설정, Customer에게 Package를 전달하면 다시 Package를 받을 수 있음

--- Assets/Delivery.cs
+++ Assets/Delivery.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Delivery : MonoBehaviour
 {
+    bool hasPackage = false;
     private void OnCollisionEnter2D(Collision2D other) 
     {
         Debug.Log("Collision!!!!!!");    
@@ -14,11 +15,13 @@ public class Delivery : MonoBehaviour
         if(other.tag == "Package")
         {
             Debug.Log("Package!!!");
+            hasPackage = true;
         }
         
-        if (other.tag == "Customer")
+        if ((other.tag == "Customer") && hasPackage)
         {
             Debug.Log("Customer!!");
+            hasPackage = false;
         }
     }
 }
