f3922babcb4d803422ba5c4d7417a24865348475
jihun90 <jihun90.kim@icloud.com>
2022-11-01 22:48:43 +0900

    Package를 하나씩만 가질수 있게 수정

--- Assets/Delivery.cs
+++ Assets/Delivery.cs
@@ -13,7 +13,7 @@ public class Delivery : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D other) 
     {
-        if(other.tag == "Package")
+        if ((other.tag == "Package") && !hasPackage)
         {
             Debug.Log("Package!!!");
             hasPackage = true;
