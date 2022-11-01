60d622cad680e8deb28184e5bf9090bf788afcfc
jihun90 <jihun90.kim@icloud.com>
2022-11-01 23:12:39 +0900

    Package를 픽업했을때 색이 바뀌도록 수정, 반납하면 다시 색이 돌아옴

--- Assets/Delivery.cs
+++ Assets/Delivery.cs
@@ -4,8 +4,18 @@ using UnityEngine;
 
 public class Delivery : MonoBehaviour
 {
+    [SerializeField] Color32 hasPackageCarColor = new Color32(200, 0, 0, 255);
+    [SerializeField] Color32 noPackageCarColor = new Color32(255, 255, 255, 255);
     [SerializeField] float destroyDelay = 0.5f;
+    
+    SpriteRenderer spriteRenderer;
     bool hasPackage = false;
+
+    private void Start() 
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
     private void OnCollisionEnter2D(Collision2D other) 
     {
         Debug.Log("Collision!!!!!!");    
@@ -17,12 +27,14 @@ public class Delivery : MonoBehaviour
         {
             Debug.Log("Package!!!");
             hasPackage = true;
+            spriteRenderer.color = hasPackageCarColor;
             Destroy(other.gameObject, destroyDelay);
         }
         
         if ((other.tag == "Customer") && hasPackage)
         {
             Debug.Log("Customer!!");
+            spriteRenderer.color = noPackageCarColor;
             hasPackage = false;
         }
     }
